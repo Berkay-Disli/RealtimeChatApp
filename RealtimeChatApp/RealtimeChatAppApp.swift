8f16d54006f6937383a12f1e57ed4ae68b1cb6b9
Berkay-Disli <berkaydisli.work@gmail.com>
2023-01-28 13:02:07 +0300

    Initial Commit

---
+++ RealtimeChatApp/RealtimeChatAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  RealtimeChatAppApp.swift
+//  RealtimeChatApp
+//
+//  Created by Berkay Disli on 28.01.2023.
+//
+
+import SwiftUI
+
+@main
+struct RealtimeChatAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
